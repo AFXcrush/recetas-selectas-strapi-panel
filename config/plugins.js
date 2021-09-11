b82f71bd0a3084f226f649d3e6b5fa299a234865
Afx <afxcrush@gmail.com>
2021-09-11 17:45:45 -0500

    cloudinary folder specified

--- config/plugins.js
+++ config/plugins.js
@@ -6,6 +6,12 @@ module.exports = ({ env }) => ({
       api_key: env("CLOUDINARY_KEY"),
       api_secret: env("CLOUDINARY_SECRET"),
     },
+    actionOptions: {
+      upload: {
+        folder: "RecetasSelectas",
+      },
+      delete: {},
+    },
   },
   graphql: {
     endpoint: "/graphql",
